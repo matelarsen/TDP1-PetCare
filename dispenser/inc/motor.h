78e93fb2af6c0a7f302e4467de1a2d0f389cc1fb
Mateo Larsen <143643490+matelarsen@users.noreply.github.com>
2025-02-13 20:21:15 -0300

    Correaciones dispensador

--- dispenser/inc/motor.h
+++ dispenser/inc/motor.h
@@ -1,9 +1,4 @@
-/*=============================================================================
- * Author: mateo
- * Date: 2025/02/06
- * Version: 1.0
- *===========================================================================*/
-
+// motor.h
 #ifndef MOTOR_H
 #define MOTOR_H
 
@@ -13,9 +8,13 @@
 #define DIR1 GPIO0
 #define DIR2 GPIO1
 #define ENABLE T_FIL2
+#define VELOCIDAD_NORMAL 128  // 50% de velocidad
+#define VELOCIDAD_REVERSA 100 // ~40% para reversa
 
 void motorInit(void);
 void activarMotor(void);
 void desactivarMotor(void);
+void motorReversa(void);
+void secuenciaDispensado(void);
 
 #endif
